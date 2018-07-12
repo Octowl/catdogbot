377ef2ed1612272b5a9c96bf136473ce153d0165
Octowl <alsaff1987@gmail.com>
2018-07-12 11:41:34 +0300

    Add logging

--- skills/reverseModeMiddleware.js
+++ skills/reverseModeMiddleware.js
@@ -1,5 +1,6 @@
 module.exports = function(controller) {
   controller.middleware.receive.use(function(bot, message, next) {
+    console.log("RCVD:" + message.text);
     controller.storage.users.get(message.user, function(err, user) {
       if (!user) {
         return next();
